89d9e31d1c96c10c9daf1286c9db43565d40edcc
scneptune <rmalcome@gmail.com>
2015-09-03 11:47:18 -0700

    still working on sorting out which route should go where and how to better setup the controllers, these in the future should be combined to respective controllers to keep the routing logic clean

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var actMatrix = require('../models/actmatrix');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -31,6 +31,44 @@ router.get('/*/interests', function (req, res, next) {
 	res.render('interests', {title: ' DatePathfinder - Interests'});
 });
 
+router.get('/tagedit', function (req, res, next) {
+	res.render('tageditor', {title: 'DatePathfinder - Secret Sauce...'});
+})
+
+router.post('/set_acts', function (req, res, next) {
+	// actMatrix.category;
+	tempArr = [];
+		for(item in req.body.filterTags) {
+			tempArr.push({filter_title: req.body.filterTags[item]});
+	}
+	var Category = new actMatrix.category({
+		desc: req.body.desc,
+		category: req.body.category,
+		filterTags: tempArr
+	});
+	// Category.filterTags.
+	// Category.filterTags
+	Category.save(function (err) {
+		if (err) res.send(err);
+			res.redirect(301, '/tagedit');
+		});
+
+});
+
+router.get('/get_acts', function (req, res, next) {
+	actMatrix.category.find(function(err, catergory) {
+   	if (err) throw err;
+  	res.send(catergory);
+   	});
+});
+
+// router.post('/interests', function (req, res, next) {
+// 	req.session;
+// 	console.error(req.body);
+// 	res.redirect(301, '/');
+// });
+
+// router.get('/');
 
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
+var Acts = require('../models/acts');
+var actMatrix = require('../models/actmatrix');
+
+router.param('id', function (req, res, next, id) {
+	console.log('found an id', id);
+	req.idVal = id;
+	next();
+})
+
+router.get('/:id', function (req, res, next) {
+	var randomIndexValue = Math.floor((Math.random() * 2) + 1);
+
+ 	User.getWithPromise({_id: req.idVal}).then(function (user) {
+ 			res.send(user);
+ 	}).catch(console.log).done();
+
+});
+
+router.post('/create', function (req, res, next) {
+	var newUser = {
+		address: req.session.address,
+		city: req.session.city,
+		location: req.session.location,
+		preferences: {
+			act1: req.body.actI_activity,
+			act2: {price: req.body.actII_spending,
+				distance: req.body.actII_distance,
+				food: req.body.actII_restaurant_type
+				},
+			act3: req.body.actIII_finale
+		}
+	};
+	createdUser = new User(newUser);
+
+	createdUser.save(function (err) {
+		if (err) res.send(err);
+		res.send('created a new user bruv.');
+	});
+
+	// {: parseFloat(req.body.spending)}
+	// var user = new User()
+});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+   User.find(function(err, user) {
+   	if (err) throw err;
+  	res.send(user);
+   });
 });
 
 module.exports = router;
